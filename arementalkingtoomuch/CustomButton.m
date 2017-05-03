7cecfd249ad72ba3453ec7c13486ad9f09aa17c1
JP Gorman <j.p.g.gorman@mgail.com>
2017-05-03 22:32:38 +0100

    Reposition label when button is active

--- arementalkingtoomuch/CustomButton.m
+++ arementalkingtoomuch/CustomButton.m
@@ -9,7 +9,10 @@
 #import "CustomButton.h"
 
 @interface CustomButton()
+@property (nonatomic, weak) UILabel *buttonLabel;
 @end
+
+static const int MARGIN = 7;
 
 @implementation CustomButton
 
@@ -30,12 +33,10 @@
 #pragma mark - drawing
 - (CGFloat)cornerRadius{ return 0.5 * self.bounds.size.width;}
 
-- (void)drawCircleWithXoffset:(int)atOffset withBackground:(UIColor*)backgroundColor {
-    
-    int offset = 7;
+- (void)drawCircleWithYoffset:(int)yOffset withBackground:(UIColor*)backgroundColor {
     
     UIBezierPath *layer = [UIBezierPath
-                           bezierPathWithRoundedRect:CGRectMake(offset, atOffset, self.bounds.size.width - offset, self.bounds.size.height - offset)
+                           bezierPathWithRoundedRect:CGRectMake(MARGIN, yOffset, self.bounds.size.width - MARGIN, self.bounds.size.height - MARGIN)
                            cornerRadius:[self cornerRadius]];
     [backgroundColor setFill];
     [layer fill];
@@ -44,29 +45,37 @@
 - (void)drawRect:(CGRect)rect {
     
     // Draw shadow
-    [self drawCircleWithXoffset:7 withBackground:[UIColor colorWithRed:0.47 green:0.47 blue:0.47 alpha:1.0]];
+    [self drawCircleWithYoffset:7 withBackground:[UIColor colorWithRed:0.47 green:0.47 blue:0.47 alpha:1.0]];
     
     // Draw button
     UIColor *backgroundColor = self.isActive
     ? [UIColor colorWithRed:0.73 green:0.76 blue:0.75 alpha:1.0]
     : [UIColor colorWithRed:0.85 green:0.89 blue:0.86 alpha:1.0];
     
-    [self drawCircleWithXoffset:self.isActive ? 5 : 0 withBackground:backgroundColor];
+    [self drawCircleWithYoffset:self.isActive ? 5 : 0 withBackground:backgroundColor];
     
-    NSLog(@"%@", self.label);
+    // Remove any existing buttonLabel
+    while ((self.buttonLabel = [[self subviews] lastObject]) != nil)
+        [self.buttonLabel removeFromSuperview];
+    
+    // Add new buttonLabel
     UILabel *label = [self drawLabel:self.label];
-    [self addSubview:label];
+    self.buttonLabel = label;
+    [self addSubview:self.buttonLabel];
     
 }
 
 - (UILabel*)drawLabel:(NSString*)title {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+    
+    int yOffset = self.isActive ? 5 : 0;
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.bounds.size.width - MARGIN, self.bounds.size.height - MARGIN)];
     [label setTextAlignment:NSTextAlignmentCenter];
     [label setTextColor:[UIColor blackColor]];
     [label setBackgroundColor:[UIColor clearColor]];
     [label setFont:[UIFont fontWithName: @"Futura" size: 16.0f]];
     [label setText:title];
-    [label setCenter:CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2)];
+    [label setCenter:CGPointMake((self.bounds.size.width / 2) + MARGIN / 2, (self.bounds.size.height / 2 + yOffset))];
     return label;
 }
 
